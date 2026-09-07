fb77d65198f5ada42b1a40a1586800816b2f35d9
Orion Fx <orionfxservis@gmail.com>
2026-09-07 22:30:39 +0500

    Add files via upload

--- mobile-apps/MyBeatBox/vite.config.ts
+++ mobile-apps/MyBeatBox/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/mobile-apps/MyBeatBox/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
